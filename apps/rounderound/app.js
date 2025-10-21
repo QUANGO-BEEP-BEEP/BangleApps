5c46b3fba276430c3216e726bf329aa4f21a7f0b
QUANGO-BEEP-BEEP <quangobeepbeep@gmail.com>
2025-10-21 11:23:57 -0400

    Add files via upload

--- apps/rounderound/app.js
+++ apps/rounderound/app.js
@@ -1,18 +1,1 @@
-/*var messageListener = Bangle.on("message", (type, message)=>{
-  //if (message.handled) return; // another app already handled this message
-  // <type> is one of "text", "call", "alarm", "map", or "music"
-  // see `messages/lib.js` for possible <message> formats
-  // message.t could be "add", "modify" or "remove"
-  E.showMessage(`${message.title}\n${message.body}`, `${message.t} ${type} message`);
-  // You can prevent the default `message` app from loading by setting `message.handled = true`:
-  message.handled = true;
-});*/
-
-/*function main()
-{
-	E.showMessage("My\nSimple\nApp","penis LOL");
-}
-
-main();*/
-
-E.showMessage("My\nSimple\nApp","penis LOL");
+E.showMessage("hello world","hello world");
